722c57c7c0ddd9cd04ad24ddb7f3dc84d53d8d40
Akshat Kushwaha <kushwaha101005@gmail.com>
2026-07-08 11:17:38 +0530

    second checkpoint

--- solution.js
+++ solution.js
@@ -1,17 +1,37 @@
 function createcard(title,cname,views,monthold){
-    document.querySelector("content").innerHTML=title;
-    document.querySelector("cname").innerHTML=cname;
-    document.querySelector("views").innerHTML=views;
-    document.querySelector("age").innerHTML=monthold;
-
-
+    document.querySelector(".title").innerHTML=title;
+    document.querySelector(".cname").innerHTML=cname;
+    document.querySelector(".views").innerHTML=views;
+    document.querySelector(".age").innerHTML=monthold;
+   
+   
 
 
 }
 
-let title=prompt("enter title")
-let cname=prompt("enter channel name")
-let views=prompt("enter views")
-let monthold=prompt("enter age")
-createcard(title,cname,views,monthold);
+function newviews(views){
+if(views>1000 && views<1000000){
+    let r=views/1000;
+    return newviews=r+"K"
+
+}
+else if(views>1000000){
+    let m=views/1000000;
+    return newviews=m+"M"
+}
+}
+    
+    
+
+
+
+
+
+
+let title=prompt("enter title");
+let cname=prompt("enter channel name");
+let views=prompt("enter views");
+let monthold=prompt("enter age");
+newviews=newviews(views)
+createcard(title,cname,newviews,monthold);
 
